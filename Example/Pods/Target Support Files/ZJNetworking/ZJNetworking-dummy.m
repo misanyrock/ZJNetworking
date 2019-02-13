2c13bfeb54e7cf06d5b83db161fd8fdb2379f22e
cxc <xchaocao@163.com>
2019-02-13 19:06:33 +0800

    init projetc

---
+++ Example/Pods/Target Support Files/ZJNetworking/ZJNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZJNetworking : NSObject
+@end
+@implementation PodsDummy_ZJNetworking
+@end
---
+++ ZJNetworking/Classes/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  FBSnapshotTestCase
+//
+//  Created by cxc on 2019/2/13.
+//
+
+import UIKit
+
+public class Person: NSObject {
+    
+    // MARK: - Property
+    public var name = "cxc"
+    
+    // MARK: - Method
+    public func say() {
+        print("say")
+    }
+}
